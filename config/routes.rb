86c5e4e855bb9015a30b8c903579f0033cc176cf
Hamish <hamisharro@icloud.com>
2021-07-08 19:29:57 +0100

    setup contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/book", to: "book#index"
+  get "/book", to: "contacts#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "home#index"
   resource :contacts, only: [:new, :create]
