a1d6305beeed9330ba9c818cf0967b94fe2134f1
Hamish <hamisharro@icloud.com>
2021-07-08 18:42:49 +0100

    make form for sending emails

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/book", to: "book#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "home#index"
+  resource :contacts, only: [:new, :create]
 end
